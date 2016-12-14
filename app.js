1a4332c954e49a98fbd1c2abcf44325475b006ae
Mike <mikeytown19@gmail.com>
2016-12-14 05:41:56 -0700

    first commit

--- app.js
+++ app.js
@@ -6,5 +6,6 @@ function handleUpdate() {
   document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
 }
 
+
 inputs.forEach(input => input.addEventListener('change', handleUpdate));
 inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
